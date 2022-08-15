5d989ff0bd44d6fe472848f2c1208c3c23a1a66a
gomesht <gomesht@hotmail.com>
2022-08-15 15:43:27 -0300

    att

---
+++ carro.py
@@ -0,0 +1,43 @@
+nome = input("Nome do piloto: ")
+vel = 0
+maxvel = 0
+horas = 0
+def main():
+    while True:
+        op = input("O que deseja fazer:\n1 - Acelerar\n2 - Desacelerar\n3 - Informações\n4 - Parar Carro:\n")
+        if op == "1":
+            acelerar_carro()
+        elif op == "2":
+            desacelerar_carro()
+        elif op == "3":
+            informacoes()
+        elif op == "4":
+            finalizar()
+            break
+
+def acelerar_carro():
+    global vel, horas, maxvel
+    if vel < 100:
+        vel += 5
+    elif vel == 100:
+        print("Você está muito rápido redzindo a velocidade para 95 km/h")
+        vel -= 5
+    horas += 1
+    if vel > maxvel:
+        maxvel = vel
+def desacelerar_carro():
+    global vel, horas
+    vel -= 5
+    if vel == 0:
+        print("O carro está parado")
+    elif vel < 0:
+        vel = 0
+    horas += 1
+def informacoes():
+    global nome, horas, vel
+    print(f"Nome do piloto: {nome}\nVelocidade atual: {vel} km/hr\nTempo de corrida: {horas} hrs")
+def finalizar():
+    global vel, horas, maxvel
+    print(f"Corrida finalizada\nDuração da corrida: {horas} hrs\nVelocidade máxima: {maxvel} km/h")
+
+main()
---
+++ funcoes.py
@@ -0,0 +1,25 @@
+"""def soma(numero): # <- Parametro
+    print(f"Estou somando o numero {numero}")
+
+soma(2) #<- Argumento"""
+"""def nome_usuario(nome, idade):
+    print(f"o nome do usuário é {nome} e a sua idade é {idade}.")
+    return (f"o nome do usuário é {nome} e a sua idade é {idade}.")
+username = str(input("Qual seu nome: "))
+userage = int(input("Qual sua idade: "))
+print(nome_usuario(username, userage).upper())"""
+
+
+#classes:
+
+# listas = ["1","2"]
+# sets = {"1","2","3","4","5"}
+# dicionarios = {id:"valor"}
+# tuplas = ("1","2") # não pode alterar valores, remover, adicionar. Um unico valor sem vírgula retorna o valor específico 
+#complexos =
+#for i in tuplas:
+# print(tuplas)
+# print(list(tuplas))
+# tuplas = list(tuplas)
+# print(tuplas)
+
